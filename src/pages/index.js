80ea9b5a6a29f450930ddc6306660b3b2bd539d7
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-02-20 16:50:15 -0500

    Feb 20 activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,16 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Form from './qcomps/updObjectsForm'
+import List from './qcomps/artistsRemoveArr'
+import BucketList from './qcomps/arrObj'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        {/* <Form /> */}
+        {/* <List /> */}
+        <BucketList />
     </div>
   )
 }
--- src/pages/qcomps/arrObj.js
+++ src/pages/qcomps/arrObj.js
@@ -10,7 +10,7 @@ const initialList = [
 export default function BucketList() {
   const [myList, setMyList] = useState(initialList);
   const [yourList, setYourList] = useState(
-    initialList
+    initialList.map(i => {return {...i}})
   );
 
   function handleToggleMyList(artworkId, nextSeen) {
