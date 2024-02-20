c5ec00ba7393b23172b44aa60491e97e432b46f9
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-02-20 13:59:54 -0500

    Feb 15 activity

--- src/pages/qcomps/state.js
+++ src/pages/qcomps/state.js
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import { sculptureList } from '../../data/data.js';
+import { useState } from "react";
+import { sculptureList } from "../../data/data.js";
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
 
   function handleNextClick() {
-    setIndex(index + 1);
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
   }
 
   function handleMoreClick() {
@@ -16,9 +20,7 @@ export default function Gallery() {
   let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handleNextClick}>
-        Next
-      </button>
+      <button onClick={handleNextClick}>Next</button>
       <h2>
         <i>{sculpture.name} </i>
         by {sculpture.artist}
@@ -27,13 +29,10 @@ export default function Gallery() {
         ({index + 1} of {sculptureList.length})
       </h3>
       <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
+        {showMore ? "Hide" : "Show"} details
       </button>
       {showMore && <p>{sculpture.description}</p>}
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
+      <img src={sculpture.url} alt={sculpture.alt} />
     </>
   );
 }
