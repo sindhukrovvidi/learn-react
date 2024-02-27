d3aad0992845cf26e640f77135a325c16047b54b
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-02-26 23:08:59 -0500

    feb 22 activity

--- src/pages/qcomps/updObjectsForm.js
+++ src/pages/qcomps/updObjectsForm.js
@@ -8,7 +8,11 @@ export default function Scoreboard() {
   });
 
   function handlePlusClick() {
-    player.likescore++;
+    setPlayer({
+      ...player,
+      likescore: player.likescore + 1
+    })
+    //player.likescore++;
   }
 
   function handleFirstNameChange(e) {
@@ -20,6 +24,7 @@ export default function Scoreboard() {
 
   function handleLastNameChange(e) {
     setPlayer({
+      ...player,
       lastName: e.target.value
     });
   }
